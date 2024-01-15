5694cfcc62d703884d63fc8131ac17c54f3e299f
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-15 12:30:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+// Codice PHP
+
+// Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. 
+// Ogni array avrà una squadra di casa e una squadra ospite, 
+// punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
+//  Stampiamo a schermo tutte le partite con questo schema:
+
+// Olimpia Milano - Cantù | 55-60
+
+
+
+
+
+$array = [
+
+    "partita01" => [
+        "Roma" => 10,
+        "Milano" => 4,
+    ],
+    "partita02" => [
+        "Napoli" => 5,
+        "Madrid" => 2,
+    ],
+    "partita03" => [
+        "Londra" => 20,
+        "Parigi" => 12,
+    ],
+    "partita04" => [
+        "Casablanca" => 33,
+        "Marrakech" => 20,
+    ],
+    "partita05" => [
+        "Bologna" => 22,
+        "Bergamo" => 43,
+    ],
+]
+
+
+
+
+?>
+
+
+<div>
+
+<ul>
+
+
+</ul>
+
+<?php var_dump($array) ?>
+
+</div>
