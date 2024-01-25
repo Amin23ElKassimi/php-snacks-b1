bfb143e9795e557457188db1e2e0f76667d17b33
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-25 12:47:18 +0100

    -Create una nuova classe che rappresenti un Pokemon e che abbia almeno cinque proprieta, tre metodi ed il costruttore.

---
+++ OOP/index.php
@@ -0,0 +1,46 @@
+<!-- Create una nuova classe che rappresenti un Pokemon e che abbia almeno cinque proprieta, tre metodi ed il costruttore.
+Microbonus: creo un'altra classe che aggiungo al pokemon per composizione che configuri il tipo di pokemon (aria, terra, ecc.) -->
+
+
+
+
+<?php
+
+class Pokemon {
+    // Proprietà
+    private $nome;
+    private $tipo;
+    private $livello;
+    private $puntiSalute;
+    private $attacco;
+
+    // Costruttore
+    public function __construct($nome, $tipo, $livello, $puntiSalute, $attacco) {
+        $this->nome = $nome;
+        $this->tipo = $tipo;
+        $this->livello = $livello;
+        $this->puntiSalute = $puntiSalute;
+        $this->attacco = $attacco;
+    }
+
+    // Metodo per ottenere il nome del Pokémon
+    public function getNome() {
+        return $this->nome;
+    }
+
+    // Metodo per ottenere i punti salute del Pokémon
+    public function getPuntiSalute() {
+        return $this->puntiSalute;
+    }
+
+    // Metodo per ottenere i punti salute del Pokémon
+    public function getTipo() {
+        return $this->tipo;
+    }
+
+
+}
+
+// Creare un'istanza di Pokemon
+$pokemon1 = new Pokemon("Pikachu", "Elettrico", 5, 50, 8);
+
